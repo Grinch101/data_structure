4f834cf5df5cc6565d5a4c30cc8ded4502837c24
Grinch101 <Masoud.Kameli@gmail.com>
2021-03-23 22:07:04 +0430

    merge-sort as a func

--- MergeSort_v2.py
+++ MergeSort_v2.py
@@ -1,45 +1,41 @@
-class MergeSort:
-    def __init__(self, arr):
 
-        self._merge_sort(arr)
-        print(arr)
+def merge_sort( arr):
+    if len(arr) >= 2:
 
-    def _merge_sort(self, arr):
-        if len(arr) >= 2:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
 
-            mid = len(arr)//2
-            left = arr[:mid]
-            right = arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
 
-            self._merge_sort(left)
-            self._merge_sort(right)
-
-            i = j = idx = 0
-            while True:
-                if left[i] <= right[j]:
-                    arr[idx] = left[i]
-                    idx += 1
-                    i += 1
-                    if i == len(left):
-                        break
-                else:
-                    arr[idx] = right[j]
-                    idx += 1
-                    j += 1
-                    if j == len(right):
-                        break
-
-            # put extra item(s) in arr
-            while i < len(left):
+        i = j = idx = 0
+        while True:
+            if left[i] <= right[j]:
                 arr[idx] = left[i]
                 idx += 1
                 i += 1
-            while j < len(right):
+                if i == len(left):
+                    break
+            else:
                 arr[idx] = right[j]
                 idx += 1
                 j += 1
+                if j == len(right):
+                    break
 
+        # put extra item(s) in arr
+        while i < len(left):
+            arr[idx] = left[i]
+            idx += 1
+            i += 1
+        while j < len(right):
+            arr[idx] = right[j]
+            idx += 1
+            j += 1
+    return arr
 
 # test:
 
-mg = MergeSort([9, 6, 4, 2, 0, 3, 1, 7, 8])
+mg = merge_sort([9, 6, 4, 2, 0, 3, 1, 7, 8])
+print(mg)
