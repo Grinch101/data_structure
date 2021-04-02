26aea0c9b7294e8ee94257372c0b50524aca3c12
Grinch101 <Masoud.Kameli@gmail.com>
2021-04-02 10:48:52 +0430

    rewritten / stopped using dot product

--- dot-multiplication.py
+++ dot-multiplication.py
@@ -1,15 +1,25 @@
+# Because each of the triply-nested for loops runs exactly n iterations, 
+# and each execution of line 7 takes constant time,
+#  the SQUARE-MATRIX-MULTIPLY procedure takes O(n^3) time.
+
+
 import numpy as np
 
 def dot_multiplication(A,B):
     
     C_nrow = A.shape[0]
     C_ncol = B.shape[1]
-    
+        
     C = np.array([0 for i in range(0, C_ncol*C_nrow)])
     C = C.reshape((C_nrow, C_ncol))
+    output = 0
     for j in range(0, C_ncol):
         for i in range(0, C_nrow ):
-            C[i,j] = sum(A[i,:]*B[:,j])
+            output = 0
+            for idx in range(A.shape[1]):
+                output = output + A[i,:][idx] * B[:,j][idx]
+            C[i,j] = output
+
     return C
 
 
